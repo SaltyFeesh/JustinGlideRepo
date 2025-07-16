7079a8acbee86f39cbaeb28b6866dd8bf50a333c
SaltyFeesh <mdzz5666@gmail.com>
2025-07-16 09:22:39 +0800

    Parallax camera
    asdasd

---
+++ Assets/CameraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Transform farBackground, middleBackground;
+    private Vector2 lastPos;
+
+
+
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        Vector2 amountToMove = new Vector2(transform.position.x - lastPos.x, transform.position.y - lastPos.y);
+
+        farBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0f);
+        middleBackground.position += new Vector3(amountToMove.x * 0.5f, amountToMove.y * 0.5f, 0f);
+
+
+        lastPos = transform.position;
+    }
+}
