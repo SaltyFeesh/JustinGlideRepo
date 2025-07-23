dd2dc99c5e84e46759ec0ed584431eda2ad41675
minnnpannn9585 <panmin677095@gmail.com>
2025-07-23 09:39:42 +0800

    0723 camera done

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,27 +5,40 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public float cameraOffsetX;
     public Transform farBackground, middleBackground;
     private Vector2 lastPos;
 
+    private float cameraSize;
+    public bool isChangingSize = false;
+    public float newSize;
+    public float cameraChangeSpeed;
 
-
-    void Start()
-    {
-     
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = new Vector3(target.position.x+cameraOffsetX, target.position.y, transform.position.z);
 
         Vector2 amountToMove = new Vector2(transform.position.x - lastPos.x, transform.position.y - lastPos.y);
 
-        //farBackground.position += new Vector3(amountToMove.x * 0.97f, amountToMove.y * 0.97f, 0f);
-        middleBackground.position += new Vector3(amountToMove.x * 0.99f, amountToMove.y * 0.99f, 0f);
+        farBackground.position += new Vector3(amountToMove.x * 0.99f, amountToMove.y * 0.99f, 0f);
+        middleBackground.position += new Vector3(amountToMove.x * 0.96f, amountToMove.y * 0.96f, 0f);
 
 
         lastPos = transform.position;
+
+        if (isChangingSize)
+        {
+            ChangeCameraSize(newSize);
+        }
+        
+    }
+
+    public void ChangeCameraSize(float newSize)
+    {
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, newSize, cameraChangeSpeed);
+        if (Mathf.Abs(newSize - Camera.main.orthographicSize) < 1f)
+        {
+            isChangingSize = false;
+        }
     }
 }
